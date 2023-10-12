f691d04f3d094516842a58200c3fb534e1c19584
Gabriela França <gabrielafranca2008@hotmail.com>
2023-10-12 20:26:11 -0300

    react-router

--- 13/SPA/src/router.jsx
+++ 13/SPA/src/router.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home"
 import Cart from "./pages/Cart"
 import AdminHome from "./pages/admin/Admin"
 import RootLayout from "./components/RootLayout"
+import Product from "./pages/Product"
+import loadProduct from "./loaders/products"
+import ProductBoundary from "./error-boundaries/ProductBoundary"
 
 const router = createBrowserRouter([
     {
@@ -16,18 +19,15 @@ const router = createBrowserRouter([
             path: "/products",
             element: <Products />
         }, {
+            path: "products/:productId",
+            element: <Product />,
+            loader: loadProduct,
+            errorElement: <ProductBoundary />
+        }, {
             path: "/cart",
             element: <Cart />
         }]
-    }, 
-    //{
-    //    path: "/products",
-    //    element: <Products />
-    //}, 
-    //{
-    //    path: "/cart",
-    //    element: <Cart />
-    //}, 
+    },
     {
         path: "/admin",
         element: <AdminHome />
